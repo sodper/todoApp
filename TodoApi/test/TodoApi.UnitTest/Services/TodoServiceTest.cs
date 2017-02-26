0fa1090abfa1cae016970fce814be8a602a9997b
Per Flitig <per.flitig@gmail.com>
2017-02-26 17:49:56 +0100

    Added failing test for getting nonexistant todo

--- TodoApi/test/TodoApi.UnitTest/Services/TodoServiceTest.cs
+++ TodoApi/test/TodoApi.UnitTest/Services/TodoServiceTest.cs
@@ -1,14 +1,23 @@
 using System;
+using FluentAssertions;
+using TodoApi.Services;
 using Xunit;
 
 namespace TodoApi.UnitTest.Services
 {
     public class TodoServiceTest
     {
-        [Fact]
-        public void Test1()
-        {
+        private TodoService _service;
 
+        public TodoServiceTest()
+        {
+            _service = new TodoService();
+        }
+
+        [Fact]
+        public void Get_NonExistingTodo_ReturnsNull()
+        {
+            _service.Get(123).Should().BeNull();
         }
     }
 }
