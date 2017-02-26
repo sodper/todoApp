64cc5167bf4a5e54e106faa193d180c0219c1855
Per Flitig <per.flitig@gmail.com>
2017-02-26 17:41:32 +0100

    Added unit testproject

---
+++ TodoApi/test/TodoApi.UnitTest/Services/TodoServiceTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace TodoApi.UnitTest.Services
+{
+    public class TodoServiceTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
