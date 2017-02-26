41d4798572c0a86df178e034bd6a4a6fd9b84941
Per Flitig <per.flitig@gmail.com>
2017-02-26 17:54:25 +0100

    Added failing not so trivial test for getting existing todo

--- TodoApi/test/TodoApi.UnitTest/Services/TodoServiceTest.cs
+++ TodoApi/test/TodoApi.UnitTest/Services/TodoServiceTest.cs
@@ -1,5 +1,6 @@
 using System;
 using FluentAssertions;
+using TodoApi.Models;
 using TodoApi.Services;
 using Xunit;
 
@@ -18,6 +19,20 @@ namespace TodoApi.UnitTest.Services
         public void Get_NonExistingTodo_ReturnsNull()
         {
             _service.Get(123).Should().BeNull();
+        }
+
+        [Fact]
+        public void Get_ExistingTodo()
+        {
+            var todo = new Todo
+            {
+                Id = 1,
+                Description = "Todo1",
+                Start = new DateTime(2017, 2, 28),
+                Due = new DateTime(2017, 3, 10)
+            };
+
+            _service.Get(1).Should().ShouldBeEquivalentTo(todo);
         }
     }
 }
